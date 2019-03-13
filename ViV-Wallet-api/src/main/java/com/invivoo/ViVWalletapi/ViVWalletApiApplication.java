5868aa3a6dbf3a7a4ad654ca6c7bcfce67c521d1
Benoît Jonathas <benoit.jonathas@gmail.com>
2019-03-13 19:08:41 +0100

    init Viv-Wallet project structure and Spring Boot ViV-Wallet-api project

---
+++ ViV-Wallet-api/src/main/java/com/invivoo/ViVWalletapi/ViVWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.invivoo.ViVWalletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViVWalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViVWalletApiApplication.class, args);
+	}
+
+}
