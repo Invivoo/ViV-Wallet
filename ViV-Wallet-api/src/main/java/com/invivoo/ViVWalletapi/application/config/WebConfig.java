913973a0b8a1701b2457e5b37ecfd25fbf99fd66
Benoît Jonathas <benoit.jonathas@gmail.com>
2019-03-14 17:30:08 +0100

    add ViV-Wallet-app project and configure pom to be able to launch ViV-Wallet-app and ViV-Wallet-api together

---
+++ ViV-Wallet-api/src/main/java/com/invivoo/ViVWalletapi/application/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.invivoo.ViVWalletapi.application.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${app.url}")
+    private String appUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(appUrl);
+    }
+
+}
