41e2419db71900c157182ae6b9cd6e95b30c9786
Marcos Almeida <marcos.almeida@xcomponent.com>
2019-12-06 10:04:25 +0100

    test: fix tests

--- ViV-Wallet-app/src/app/admin-edit-user/admin-edit-user.component.spec.ts
+++ ViV-Wallet-app/src/app/admin-edit-user/admin-edit-user.component.spec.ts
@@ -1,7 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { AdminEditUserComponent } from './admin-edit-user.component';
-import { DxButtonModule, DxTextBoxModule } from 'devextreme-angular';
+import { DxButtonModule, DxTextBoxModule, DxValidatorModule } from 'devextreme-angular';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+
+const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
 describe('AdminEditUserComponent', () => {
     let component: AdminEditUserComponent;
@@ -10,7 +15,16 @@ describe('AdminEditUserComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [AdminEditUserComponent],
-            imports: [DxButtonModule, DxTextBoxModule]
+            imports: [
+                DxButtonModule,
+                DxTextBoxModule,
+                DxValidatorModule,
+                HttpClientTestingModule
+            ],
+            providers: [
+                { provide: Router, useValue: routerSpy },
+                { provide: ActivatedRoute, useValue: { snapshot: { params: { id: '123' } } } }
+            ]
         })
             .compileComponents();
     }));
--- ViV-Wallet-app/src/app/users.service.spec.ts
+++ ViV-Wallet-app/src/app/users.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UsersService } from './users.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('UsersService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+        });
+    });
 
-  it('should be created', () => {
-    const service: UsersService = TestBed.get(UsersService);
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        const service: UsersService = TestBed.get(UsersService);
+        expect(service).toBeTruthy();
+    });
 });
