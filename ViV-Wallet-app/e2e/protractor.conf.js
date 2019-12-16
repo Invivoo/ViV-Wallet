f12b205408f7092b829426e0d9b9ee3cb6b6ae47
nicolaserny <nicolas.erny@xcomponent.com>
2019-12-16 12:26:44 +0100

    Configure VSCode remote development with containers https://code.visualstudio.com/docs/remote/remote-overview => we can start the project in a sandbox environment (not need to install java or nodejs locally) => same environment as circleci

--- ViV-Wallet-app/e2e/protractor.conf.js
+++ ViV-Wallet-app/e2e/protractor.conf.js
@@ -9,7 +9,10 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ["--headless", "--disable-gpu", "--window-size=800x600"]
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
@@ -17,7 +20,7 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () { }
   },
   onPrepare() {
     require('ts-node').register({
