8f6c63f6611c7457da03473a45fdd9ed29352df1
Marcos Almeida <marcos.almeida@xcomponent.com>
2019-12-17 19:53:10 +0100

    edit user: only save when form fields are valid

--- ViV-Wallet-app/src/app/admin-edit-user/admin-edit-user.component.spec.ts
+++ ViV-Wallet-app/src/app/admin-edit-user/admin-edit-user.component.spec.ts
@@ -5,8 +5,16 @@ import { AdminEditUserComponent } from './admin-edit-user.component';
 import { DxButtonModule, DxTextBoxModule, DxValidatorModule } from 'devextreme-angular';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ActivatedRoute } from '@angular/router';
+import { UsersService } from '../users.service';
+import { of } from 'rxjs';
+import { User } from '../types';
 
 const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+const saveUserSpy = jasmine.createSpyObj(['subscribe']);
+const usersSpy = jasmine.createSpyObj('UsersService', {
+    saveUser: saveUserSpy,
+    getUser: jasmine.createSpyObj({ subscribe: of(new User()) })
+});
 
 describe('AdminEditUserComponent', () => {
     let component: AdminEditUserComponent;
@@ -23,6 +31,7 @@ describe('AdminEditUserComponent', () => {
             ],
             providers: [
                 { provide: Router, useValue: routerSpy },
+                { provide: UsersService, useValue: usersSpy },
                 { provide: ActivatedRoute, useValue: { snapshot: { params: { id: '123' } } } }
             ]
         })
@@ -38,4 +47,28 @@ describe('AdminEditUserComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should not save when form is not valid', () => {
+        let event = {
+            validationGroup: {
+                validate: () => { return { isValid: false }; }
+            },
+        };
+
+        component.confirm(event);
+        expect(saveUserSpy.subscribe).not.toHaveBeenCalled();
+    });
+
+    it('should  save when form is  valid', () => {
+        let event = {
+            validationGroup: {
+                validate: () => { return { isValid: true }; }
+            },
+        };
+
+        component.confirm(event);
+        expect(saveUserSpy.subscribe).toHaveBeenCalled();
+    });
+
+
 });
--- ViV-Wallet-app/src/app/admin-edit-user/admin-edit-user.component.ts
+++ ViV-Wallet-app/src/app/admin-edit-user/admin-edit-user.component.ts
@@ -25,9 +25,12 @@ export class AdminEditUserComponent implements OnInit {
         this.initUser(id);
     }
 
-    confirm() {
-        this.usersService.saveUser(this.user)
-            .subscribe(() => this.router.navigate(['/users']));
+    confirm(event) {
+        let result = event.validationGroup.validate();
+        if (result.isValid) {
+            this.usersService.saveUser(this.user)
+                .subscribe(() => this.router.navigate(['/users']));
+        }
     }
 
     private initUser(id: string) {
