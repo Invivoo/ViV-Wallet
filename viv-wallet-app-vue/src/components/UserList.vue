a95aaf4f1d82c69d2a204d05df51c460a6c12886
nicolaserny <nicolas.erny@xcomponent.com>
2019-12-26 17:38:01 +0100

    Add mock server to vuejs app + users view skeleton

---
+++ viv-wallet-app-vue/src/components/UserList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>user list</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class UserList extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ viv-wallet-app-vue/src/views/Users.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="users">
+        <UserList />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import UserList from "@/components/UserList.vue";
+
+export default {
+    name: "users",
+    components: {
+        UserList
+    }
+};
+</script>
