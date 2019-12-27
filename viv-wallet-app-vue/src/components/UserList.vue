d020c8989a45b91eed0eb5eef6b10b8a0856c895
nicolaserny <nicolas.erny@xcomponent.com>
2019-12-27 11:16:38 +0100

    Create basic Users view - Use axios to get users from backend

--- viv-wallet-app-vue/src/components/UserList.vue
+++ viv-wallet-app-vue/src/components/UserList.vue
@@ -1,14 +1,27 @@
 <template>
     <div>
-        <h1>user list</h1>
+        <ol>
+            <li v-for="user in users" :key="user.id">
+                {{ user.login }}
+            </li>
+        </ol>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import axios from "axios";
+import { User } from "../models/user";
+import { BACKEND_BASE_URL } from "../config/constants";
 
 @Component
-export default class UserList extends Vue {}
+export default class UserList extends Vue {
+    users: User[] = [];
+
+    mounted() {
+        axios.get<User[]>(`${BACKEND_BASE_URL}/users`).then(response => (this.users = response.data));
+    }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ viv-wallet-app-vue/src/models/user.ts
@@ -0,0 +1,6 @@
+export interface User {
+    id: string;
+    login: string;
+    fullname: string;
+    email: string;
+}
