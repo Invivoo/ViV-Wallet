021fe776fce23a20c3d74fe91512d5387a01fe63
Marcos Almeida <marcos.almeida@xcomponent.com>
2020-01-05 09:59:11 +0100

    migration: banner and login service

--- viv-wallet-app-vue/src/App.vue
+++ viv-wallet-app-vue/src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="app">
+        <Banner></Banner>
         <div id="nav"><router-link to="/">Home</router-link> | <router-link to="/users">Users</router-link></div>
         <router-view />
     </div>
--- viv-wallet-app-vue/src/main.ts
+++ viv-wallet-app-vue/src/main.ts
@@ -4,10 +4,11 @@ import router from "./router";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import Banner from "./components/Banner.vue";
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
-
+Vue.component("Banner", Banner);
 new Vue({
     router,
     render: h => h(App)
