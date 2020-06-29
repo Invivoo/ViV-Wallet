c2be5ab3f9402d9cde0617bc23694f52f8ccbf91
Marcos Almeida <marcos.almeida@xcomponent.com>
2020-06-29 19:22:52 +0200

    app: x4b-ui: basic integration

--- viv-wallet-app/src/App.vue
+++ viv-wallet-app/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-    <div id="app">
-        <Banner></Banner>
-        <div id="nav"><router-link to="/">Home</router-link> | <router-link to="/users">Users</router-link></div>
-        <router-view />
-    </div>
+<div id="app">
+  <x4b-ui application="scenario"
+          appsServiceUrl="http://apps.master.496a8aa9ae8c48329209.northeurope.aksapp.io/applications"
+          version="version"
+          color="#5e2572">
+    <Banner></Banner>
+    <div id="nav"><router-link to="/">Home</router-link> | <router-link to="/users">Users</router-link></div>
+    <router-view />
+  </x4b-ui>
+</div>
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
