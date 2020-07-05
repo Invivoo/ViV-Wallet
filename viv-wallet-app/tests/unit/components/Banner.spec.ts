898e6cea1d427449f509bbaf7ca98376106ec747
Marcos Almeida <marcos.almeida@xcomponent.com>
2020-07-05 12:39:16 +0200

    Banner.spec.ts: remove old features

--- viv-wallet-app/tests/unit/components/Banner.spec.ts
+++ viv-wallet-app/tests/unit/components/Banner.spec.ts
@@ -2,19 +2,9 @@ import Banner from "@/components/Banner.vue";
 import { shallowMount } from "@vue/test-utils";
 
 describe("Banner", () => {
-    it("renders logout button", () => {
-        const wrapper = shallowMount(Banner);
-        expect(wrapper.text().toLowerCase()).toContain("logout");
-    });
-
-    it("renders user name", async () => {
-        const wrapper = shallowMount(Banner);
-        expect(wrapper.text().toLowerCase()).toContain("montgomery");
-    });
-
     it("renders user's role", async () => {
         const wrapper = shallowMount(Banner);
         await wrapper.vm.$nextTick();
-        expect(wrapper.text().toLowerCase()).toContain("administrator");
+        expect(wrapper.text().toLowerCase()).toContain("admin");
     });
 });
