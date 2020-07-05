d44e71d738f3daf282e57bdab5e141a7e0536273
Marcos Almeida <marcos.almeida@xcomponent.com>
2020-07-05 12:31:40 +0200

    constants.ts: fix urls

--- viv-wallet-app/src/config/constants.ts
+++ viv-wallet-app/src/config/constants.ts
@@ -1,4 +1,4 @@
 export const BACKEND_BASE_URL = process.env.NODE_ENV === "production" ? "/api/v1" : "http://localhost:3000/api/v1";
 export const REQUEST_TIMEOUT_MS = 10000;
-export const LOGIN_URL = "http://login.master.496a8aa9ae8c48329209.northeurope.aksapp.io";
-export const APPS_URL = "http://apps.master.496a8aa9ae8c48329209.northeurope.aksapp.io";
+export const LOGIN_URL = "http://login.x4b.496a8aa9ae8c48329209.northeurope.aksapp.io";
+export const APPS_URL = "http://apps.x4b.496a8aa9ae8c48329209.northeurope.aksapp.io";
