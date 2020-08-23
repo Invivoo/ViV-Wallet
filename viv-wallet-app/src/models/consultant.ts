c7cab53410608dcede0e23bee0a6c541aa95e472
Marcos Almeida <marcos.almeida@xcomponent.com>
2020-08-23 17:11:07 +0200

    issue31: refacto: move names of statuses

--- viv-wallet-app/src/models/consultant.ts
+++ viv-wallet-app/src/models/consultant.ts
@@ -11,3 +11,16 @@ export enum ConsultantStatus {
     CONSULTANT_SENIOR_IN_ONBOARDING,
     MANAGER
 }
+
+export function toString(status: ConsultantStatus): string {
+    switch (status) {
+        case ConsultantStatus.CONSULTANT_SENIOR:
+            return "Senior";
+        case ConsultantStatus.MANAGER:
+            return "Manager";
+        case ConsultantStatus.CONSULTANT_SENIOR_IN_ONBOARDING:
+            return "Onboarding";
+        default:
+            return "Inconnu";
+    }
+}
