2fc3982cc0a0ec5545332558b8bd72ff77341a7d
cgodard <cgodard@financeactive.com>
2021-05-08 22:44:00 +0200

    Fix an error in computing VIV for a winning coaching (#162)

--- ViV-Wallet-api/src/test/java/com/invivoo/vivwallet/api/infrastructure/lynx/LynxConnectorTest.java
+++ ViV-Wallet-api/src/test/java/com/invivoo/vivwallet/api/infrastructure/lynx/LynxConnectorTest.java
@@ -1,12 +1,17 @@
 package com.invivoo.vivwallet.api.infrastructure.lynx;
 
 
+import com.invivoo.vivwallet.api.domain.action.Action;
+import com.invivoo.vivwallet.api.domain.user.User;
+import com.invivoo.vivwallet.api.domain.user.UserService;
+import com.invivoo.vivwallet.api.infrastructure.lynx.mapper.ActivityToActionMapper;
 import com.invivoo.vivwallet.api.infrastructure.lynx.model.Activity;
 import com.invivoo.vivwallet.api.infrastructure.lynx.model.ActivityType;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
@@ -28,6 +33,11 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static java.util.Collections.emptySet;
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 @RunWith(SpringRunner.class)
 @Import(LynxConnectorConfiguration.class)
 @PropertySource("classpath:application.yml")
@@ -35,6 +45,10 @@ public class LynxConnectorTest {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Mock
+    private UserService userService;
+
     private MockRestServiceServer mockServer;
 
     @Before
@@ -77,5 +91,46 @@ public class LynxConnectorTest {
 
         Assert.assertTrue(actualActivity.isPresent());
         Assert.assertEquals(expectedActivity, actualActivity.get());
+    }
+
+    @Test
+    public void should_get_2_actions_with_10_and_40_viv_for_a_winning_coaching_done_by_a_single_collaborator() throws URISyntaxException, IOException {
+        // Given
+        when(userService.findByFullName("Collaborateur1")).thenReturn(
+                Optional.of(new User(1L, "Collaborateur1", "Collaborateur1", emptySet(), emptySet())));
+        LynxConnector lynxConnector = new LynxConnector(restTemplate, "http://vivApiUrl", new ActivityToActionMapper(userService));
+
+        Path lynxResponse = Paths.get(Objects.requireNonNull(getClass().getClassLoader().getResource("lynx-response-winning-coaching-1-collaborator.json")).toURI());
+        String expectedLynxResponse = Files.lines(lynxResponse).collect(Collectors.joining());
+        mockServer.expect(MockRestRequestMatchers.anything()).andRespond(MockRestResponseCreators.withSuccess(expectedLynxResponse, MediaType.APPLICATION_JSON_UTF8));
+
+        // When
+        List<Action> actions = lynxConnector.findActions();
+
+        // Then
+        assertThat(actions).extracting(Action::getVivAmount).containsExactlyInAnyOrder(10, 40);
+    }
+
+    @Test
+    public void should_get_2_actions_with_10_and_20_viv_by_collaborator_for_a_winning_coaching_done_by_2_collaborators() throws URISyntaxException, IOException {
+        // Given
+        User collaborator1 = new User(1L, "Collaborateur1", "Collaborateur1", emptySet(), emptySet());
+        when(userService.findByFullName(collaborator1.getFullName())).thenReturn(Optional.of(collaborator1));
+        User collaborator2 = new User(2L, "Collaborateur2", "Collaborateur2", emptySet(), emptySet());
+        when(userService.findByFullName(collaborator2.getFullName())).thenReturn(Optional.of(collaborator2));
+        LynxConnector lynxConnector = new LynxConnector(restTemplate, "http://vivApiUrl", new ActivityToActionMapper(userService));
+
+        Path lynxResponse = Paths.get(Objects.requireNonNull(getClass().getClassLoader().getResource("lynx-response-winning-coaching-2-collaborators.json")).toURI());
+        String expectedLynxResponse = Files.lines(lynxResponse).collect(Collectors.joining());
+        mockServer.expect(MockRestRequestMatchers.anything()).andRespond(MockRestResponseCreators.withSuccess(expectedLynxResponse, MediaType.APPLICATION_JSON_UTF8));
+
+        // When
+        List<Action> actions = lynxConnector.findActions();
+
+        // Then
+        List<Action> collaborator1Actions = actions.stream().filter(action -> action.getAchiever().equals(collaborator1)).collect(toList());
+        assertThat(collaborator1Actions).extracting(Action::getVivAmount).containsExactlyInAnyOrder(10, 20);
+        List<Action> collaborator2Actions = actions.stream().filter(action -> action.getAchiever().equals(collaborator2)).collect(toList());
+        assertThat(collaborator2Actions).extracting(Action::getVivAmount).containsExactlyInAnyOrder(10, 20);
     }
 }
