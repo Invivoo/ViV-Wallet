58815904870223d626bfb546874afe3b3e06a277
nicolaserny <nicolas.erny@xcomponent.com>
2022-11-23 15:05:37 +0100

    Add maintenance mode option

--- viv-wallet-app/src/main.ts
+++ viv-wallet-app/src/main.ts
@@ -1,10 +1,15 @@
 import { createApp } from "vue";
 import { applyPolyfills, defineCustomElements } from "x4b-ui/loader";
 import App from "./App.vue";
+import Maintenance from "./Maintenance.vue";
 import router from "./router";
 
 applyPolyfills().then(() => {
     defineCustomElements();
 });
 
-createApp(App).use(router).mount("#app");
+if (process.env.VUE_APP_MAINTENANCE_MODE === "true") {
+    createApp(Maintenance).mount("#app");
+} else {
+    createApp(App).use(router).mount("#app");
+}
