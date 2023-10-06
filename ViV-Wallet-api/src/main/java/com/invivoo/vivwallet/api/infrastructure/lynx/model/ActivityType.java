e2444db0d5f399a0b1085865e62f082f4776e49d
OlvChv <38701064+OlvChv@users.noreply.github.com>
2023-10-06 15:23:28 +0200

    Remise des enums dans l'ordre (#229)
    Co-authored-by: OlvChv <olivier.charve@invivoo.com>

--- ViV-Wallet-api/src/main/java/com/invivoo/vivwallet/api/infrastructure/lynx/model/ActivityType.java
+++ ViV-Wallet-api/src/main/java/com/invivoo/vivwallet/api/infrastructure/lynx/model/ActivityType.java
@@ -25,21 +25,16 @@ public enum ActivityType {
 
     INITIALISATION_DUN_PARTENARIAT("Partnership:Accord"),
 
-    CREATION_DE_SUPPORT_DE_FORMATION_2H_HTT("Training:Supports2H-HTT"),
-    CREATION_DE_SUPPORT_DE_FORMATION_1J("Training:Supports1J"),
-    CREATION_DE_SUPPORT_DE_FORMATION_1J_HTT("Training:Supports1J-HTT"),
-    ANIMATION_SESSION_DE_FORMATION_2H_HTT("Training:Session2H-HTT"),
-    ANIMATION_SESSION_DE_FORMATION_1J("Training:Session1J"),
-    ANIMATION_SESSION_DE_FORMATION_WEBINAR("Training:Webinar"),
+    CREATION_DE_SUPPORT_DE_FORMATION("Training:Supports"),
+    ANIMATION_DUNE_SESSION_DE_FORMATION_EN_PRESENTIEL("Training:Session"),
+    ANIMATION_DUNE_SESSION_DE_FORMATION_AU_FORMAT_WEBINAR("Training:Webinar"),
     PARTICIPATION_A_UNE_CONFERENCE_EN_TANT_QUE_SPEAKER("Event:Talk"),
-    RETEX("Event:RETEX"),
 
     PUBLIER_UN_ARTICLE_COURT("Publish:Article court"),
     PUBLIER_UN_ARTICLE_MOYEN("Publish:Article moyen"),
     PUBLIER_UN_ARTICLE_LONG("Publish:Article long"),
     PUBLIER_UN_LIVRE_BLANC("Publish:Livre blanc"),
     PUBLIER_UN_CHEAT_SHEET("Publish:Cheat Sheet"),
-    INTERVIEW("Publish:Interview"),
 
     AUDIT_CIR_PHASE_1("Audit R&D - phase 1"),
     AUDIT_CIR_PHASE_2("Audit R&D - phase 2"),
@@ -47,16 +42,24 @@ public enum ActivityType {
     DEV_SMALL("Dev:Small"),
     DEV_MEDIUM("Dev:Medium"),
     DEV_LARGE("Dev:Large"),
-    DEV_EXTRA_LARGE("Dev:XLarge");
+    DEV_EXTRA_LARGE("Dev:XLarge"),
+
+    CREATION_DE_SUPPORT_DE_FORMATION_2H_HTT("Training:Supports2H-HTT"),
+    CREATION_DE_SUPPORT_DE_FORMATION_1J("Training:Supports1J"),
+    CREATION_DE_SUPPORT_DE_FORMATION_1J_HTT("Training:Supports1J-HTT"),
+    ANIMATION_SESSION_DE_FORMATION_2H_HTT("Training:Session2H-HTT"),
+    ANIMATION_SESSION_DE_FORMATION_1J("Training:Session1J"),
+    RETEX("Event:RETEX"),
+    INTERVIEW("Publish:Interview");
 
     private final String name;
 
     @JsonCreator
     public static ActivityType forValue(String value) {
         return Stream.of(ActivityType.values())
-                     .filter(a -> a.getName().equals(value))
-                     .findFirst()
-                     .orElse(null);
+                .filter(a -> a.getName().equals(value))
+                .findFirst()
+                .orElse(null);
     }
 
     @JsonValue
